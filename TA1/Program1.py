f90378100fe00545424eb6d5aa1c0572c2f4d1e5
nicolenicole123 <202311593@fit.edu.ph>
2025-02-02 16:14:04 +0800

    Update Program1.py

--- TA1/Program1.py
+++ TA1/Program1.py
@@ -1,24 +1,23 @@
-def count_characters(s):
-    vowels = "aeiouAEIOU"
-    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
-    
-    vowel_count = consonant_count = space_count = other_count = 0
+vowels = "aeiouAEIOU"
+consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 
-    for char in s:
-        if char in vowels:
-            vowel_count += 1
-        elif char in consonants:
-            consonant_count += 1
-        elif char == " ":
-            space_count += 1
-        else:
-            other_count += 1
+vowel_count = consonant_count = space_count = other_count = 0
 
-    print(f"Vowels: {vowel_count}")
-    print(f"Consonants: {consonant_count}")
-    print(f"Spaces: {space_count}")
-    print(f"Other Characters: {other_count}")
+s = input("Enter a string: ")
 
-# Example usage
-input_str = input("Enter a string: ")
-count_characters(input_str)
+for char in s:
+    if char in vowels:
+        vowel_count += 1
+    elif char in consonants:
+        consonant_count += 1
+    elif char == " ":
+        space_count += 1
+    else:
+        other_count += 1
+
+print("Vowels: {vowel_count}")
+print(f"Consonants: {consonant_count}")
+print(f"Spaces: {space_count}")
+print(f"Other Characters: {other_count}")
+
+
